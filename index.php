ffc64efa1e8db6b6c6f391c2e505ce7632ef1974
ninikoru <152066514+ninikoru@users.noreply.github.com>
2024-02-25 21:55:39 +0800

    NOTE TO JOYCE: sa may line 318 ng random.php
    sa line 318 ng random.php mo ilalagay logic code muu

--- index.php
+++ index.php
@@ -8,21 +8,24 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IM+Fell+English:ital@0;1&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>DEAL OR DEAD</title>
 </head>
 <body>
     
     <!-- <img src='images/Deal or Dead front.png' class='front'> -->
 
-    <img src='images/DoD_front_title.png' class='front-title'>
+    <img src='images/DoD_title.png' class='front-title'>
 
 
     <form action="random.php" method="post">
-    <button type="submit" class="btn"><b><img src='images/DoD_front_play.png'></b></button>
-    <img src='images/DoD_front_exit.png'>
+    <button type="submit" class="btn"><b><img src='images/DoD_play.png'></b></button>
+    <img src='images/DoD_exit.png'>
+    <img src='images/DoD_mechanics.png'>
     </form>
 
-    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- random.php
+++ random.php
@@ -8,7 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IM+Fell+English:ital@0;1&display=swap" rel="stylesheet">  
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>DEAL OR DEAD</title>
 </head>
 <body>
     
@@ -19,8 +20,6 @@
         $card1 = (rand(1,13));
         $card2 = (rand(1,13));
         $card3 = (rand(1,13));
-
-        echo "$card1, $card2";
 
         //CARD 1
 
@@ -182,19 +181,143 @@
 
         }
 
-        if ($card1 != $card2) {
+        //CARD 3
 
-            echo"
-            <form action='random.php' method='post'>
-            <button type='submit' class='btn-deal'><b><img src='images/DoD_deal.png'></b></button>
-            <button type='submit' class='btn-dead'><b><img src='images/DoD_dead.png'></b></button>
-            </form>
-            ";
+        if ($card3 == 1) {
+
+            echo "<img src='images/DoD_A.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
         }
 
+        else if ($card3 == 2) {
+
+            echo "<img src='images/DoD_2.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 3) {
+
+            echo "<img src='images/DoD_3.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 4) {
+
+            echo "<img src='images/DoD_4.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 5) {
+
+            echo "<img src='images/DoD_5.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 6) {
+
+            echo "<img src='images/DoD_6.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 7) {
+
+            echo "<img src='images/DoD_7.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 8) {
+
+            echo "<img src='images/DoD_8.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 9) {
+
+            echo "<img src='images/DoD_9.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 10) {
+
+            echo "<img src='images/DoD_10.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 11) {
+
+            echo "<img src='images/DoD_joker.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 12) {
+
+            echo "<img src='images/DoD_queen.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+        else if ($card3 == 13) {
+
+            echo "<img src='images/DoD_king.jpg' id='img-card3' class='img-card' width='320' height='470'>";
+
+        }
+
+//IF CARD 1 AND CARD 2 ARE NOT EQUAL
+
+        if ($card1 != $card2) {
+
+        echo "
+        <button type='button' onclick='dealShowAndCheck()' id='img-deal' class='btn-deal'><b><img src='images/DoD_deal.png'></b></button>
+        <img src='images/DoD_or.png' id='img-or'>
+        <button type='button' onclick='deadShowAndCheck()' id='img-dead' class='btn-dead'><b><img src='images/DoD_dead.png'></b></button>
+        ";
+
+        echo "<div id='result'></div>";
+
+        echo "
+        <script>
+            function dealShowAndCheck() {
+                var card1 = $card1;
+                var card2 = $card2;
+                var card3 = $card3;
+
+                document.getElementById('img-card3').style.display = 'block';    
+                document.getElementById('img-deal').style.display = 'none';
+                document.getElementById('img-or').style.display = 'none';    
+                document.getElementById('img-dead').style.display = 'none';
+
+                if (card3 > card1 && card3 < card2 || card3 < card1 && card3 > card2) {
+                    document.getElementById('result').innerHTML = 'YOU WIN';
+                } else {
+                    document.getElementById('result').innerHTML = 'YOU LOSE';
+                }
+            }
+            
+            function deadShowAndCheck() {
+                var card1 = $card1;
+                var card2 = $card2;
+                var card3 = $card3;
+
+                document.getElementById('img-card3').style.display = 'block';    
+                document.getElementById('img-deal').style.display = 'none';
+                document.getElementById('img-or').style.display = 'none';    
+                document.getElementById('img-dead').style.display = 'none';
+
+                if (card3 > card1 && card3 < card2 || card3 < card1 && card3 > card2) {
+                    document.getElementById('result').innerHTML = 'SHOULD HAVE JUST DEALT WITH IT';
+                } else {
+                    document.getElementById('result').innerHTML = 'NICE, STAY DEAD';
+                }
+            }
+        </script>
+ 
+        "; 
+        }
+
+//IF CARD 1 AND CARD 2 ARE EQUAL
+
+        //INSERT JOYCE'S CODE
+
     }
-
-
 
     else {
         header("Location: index.php");
@@ -203,6 +326,7 @@
     
     ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
